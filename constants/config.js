046e697a890c0c3882c63e33ffbc434855e53c4d
sauravatgithub-web <22cs01010@iitbbs.ac.in>
2024-10-05 19:14:05 +0530

    fixing CORS

--- constants/config.js
+++ constants/config.js
@@ -2,4 +2,5 @@ export const corsOptions = {
     origin: [ "http://localhost:5173", "http://localhost:4173", process.env.CLIENT_URL, "https://moviecom-client.vercel.app/" ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
 }
